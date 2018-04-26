16a959f5e71f93a800cc586464aa7bc138b0456d
Precious Humphrey <phumphrey23@gmail.com>
2018-04-26 22:08:30 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
+def sort_array_asc(array)
+ array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort.reverse do |a, b|
+     a <=> b
+   end
+end
+
+def sort_array_char_count(array)
+  array.sort{|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array.sort{|a, b| a[1] <=> b[2]}
+end
+
+def reverse_array
+
+end
+
+def kesha_maker
+
+end
+
+def find_a
+
+end
+
+def sum_array
+
+end
+
+def add_s
+
+end
